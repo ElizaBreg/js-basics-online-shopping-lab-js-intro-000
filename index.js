3929fe1db9577d31d8fdfec8c0adfbf6ef9610ec
Elizabeth <elizbregman@gmail.com>
2020-07-29 19:23:30 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -10,21 +10,66 @@ function setCart(c) {
 }
 
 function addToCart(item) {
- // write your code here
-}
+  let price = Math.floor(Math.random()*100);
+  let newCart = {itemName:item, itemPrice:price}
+  cart.push(newCart);
+  return(`${item} has been added to your cart.`);
+  }
 
 function viewCart() {
-  // write your code here
+
+  if (cart.length === 0) {
+    return("Your shopping cart is empty.")
+  }
+
+  let totalCartItems = "";
+  let beginning = "In your cart, you have ";
+  totalCartItems.push(beginning)
+
+  if (cart.length === 1) {
+        totalCartItems.push(`${Object.keys(cart[0])} at $${Object.values(cart[0])}.`)
+        return totalCartItems;
+  }
+
+  if (cart.length === 2) {
+            totalCartItems.push(`${Object.keys(cart[0])} at $${Object.values(cart[0])}, and ${Object.keys(cart[1])} at $${Object.values(cart[1])}`)
+            return totalCartItems;
+  }
+if (cart.length > 2) {
+  for (let i=0; i<cart.length-1; i++) {
+    totalCartItems.push(`${Object.keys(cart[i])} at $${Object.values(cart[i])}`)
+  }
+    totalCartItems.join(" ,")
+    totalCartItems.push(`${Object.keys(cart[cart.length-1])} at $${Object.values(cart[cart.length-1])}.`)
+    return totalCartItems;
+}
 }
 
 function total() {
-  // write your code here
+  let totalCost = 0;
+  for(let i=0; i < cart.length; i++) {
+    totalCost += cart[i][price]
+  }
+return totalCost;
 }
 
+
 function removeFromCart(item) {
-  // write your code here
+  for(let i=0; i < cart.length; i++) {
+    if(cart[i].hasOwnProperty(item)) {
+      cart.splice(i,1);
+      return cart;
+  } else {
+    return "That item is not in your cart."
+  }
+}
 }
 
 function placeOrder(cardNumber) {
-  // write your code here
+  if (cardNumber === undefined || cardNumber === 0) {
+return(`Sorry, we don't have a credit card on file for you.`)
+} else {
+  cart = [];
+  return(`Your total cost is $${total()}, which will be charged to the card ${cardNumber}.`)
+}
 }
